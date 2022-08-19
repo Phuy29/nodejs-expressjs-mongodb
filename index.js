8580c1c6f1e4b467e3a760868f75a218c1d85afe
Phuy <phuchuy292003@gmail.com>
2022-08-19 23:21:40 +0700

    Session 4

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const express = require("express");
 const app = express();
 
-const studentRouter = require("./students.js");
+const jwt = require("jsonwebtoken");
 
-app.use("/student", studentRouter);
+// app.post("/login", (req, res) => {
+//   const user = login();
+//   const token = jwt.sign({ username: user.username }, "PRIVATE_KEY");
+//   res.json({ user: user, token: token });
+// });
 
-const log = (req, res, next) => {
-  console.log("New req app at", new Date());
-  next();
+// app.post("/students", (req, res) => {
+//   try {
+//     const token = req.headers.authorization.split(" ")[1];
+//     const decoded = jwt.verify(token, "PRIVATE_KEY");
+//     if (decoded) {
+//       res.json(students);
+//     }
+//   } catch (err) {
+//     res.status(401).send("Invalid Token");
+//   }
+// });
+
+const jwtMdw = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    var decoded = jwt.verify(token, "PRIVATE_KEY");
+    if (decoded) {
+      req.username = decoded.username;
+      next();
+    }
+  } catch (err) {
+    res.status(401).send("Invalid Token");
+  }
 };
 
-app.use(log);
+app.post("/students", jwtMdw, (req, res) => {
+  console.log(req.username);
+  res.json(students);
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.json({ name: "Huy", age: 20 });
 });
 
 app.listen("8000", (req, res) => {
-  console.log("Server is running ...");
+  console.log("Server is running ... ");
 });
